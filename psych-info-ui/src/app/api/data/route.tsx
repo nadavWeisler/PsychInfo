818b93be5f803297b5482b52027b009158fd3523
Nadav183 <nadav183@gmail.com>
2023-10-09 12:12:36 +0300

    Added route for posting a new tag general clean up

--- psych-info-ui/src/app/api/data/route.tsx
+++ psych-info-ui/src/app/api/data/route.tsx
@@ -1,22 +1,13 @@
-import {readJsonFile} from "@/app/api/utils";
+import {readJsonFile, writeJsonFile, parseParams, DATAFILE} from "@/app/api/utils";
 export function GET(request: any) {
-
-    const paramsString = request.url.split("?")[1]
-    const eachParamArray = paramsString.split('&');
-    let params = {};
-    eachParamArray.forEach((param: string) => {
-        const key = param.split('=')[0];
-        params[key] = param.split('=')[1];
-    })
+    const params: {} = parseParams(request);
+    // @ts-ignore
     let tags = params["tags"].split(',')
-    const data = readJsonFile('C:\\Users\\User\\WebstormProjects\\PsychInfo\\psych-info-ui\\src\\data\\data.json');
+    const data = readJsonFile(DATAFILE);
     let res = [];
     for (let key in data) {
-        console.log(key)
         for (let j = 0; j < tags.length; j++) {
-            console.log(tags[j])
             if (data[key]['tags'].split(",").includes(tags[j])) {
-                console.log(data[key]['tags'])
                 res.push(data[key]);
             }
         }
--- psych-info-ui/src/app/api/utils.ts
+++ psych-info-ui/src/app/api/utils.ts
@@ -4,3 +4,22 @@ export function readJsonFile(filePath: string) {
     const file = fs.readFileSync(filePath);
     return JSON.parse(file.toString());
 }
+
+export function writeJsonFile(filePath: string, data: any) {
+    fs.writeFileSync(filePath, JSON.stringify(data));
+}
+
+export function parseParams(request: any) {
+    const paramsString = request.url.split("?")[1]
+    const eachParamArray = paramsString.split('&');
+    let params = {};
+    eachParamArray.forEach((param: string) => {
+        const key = param.split('=')[0];
+        // @ts-ignore
+        params[key] = param.split('=')[1];
+    })
+    return params;
+}
+
+export const DATAFILE = 'C:\\Users\\User\\WebstormProjects\\PsychInfo\\psych-info-ui\\src\\data\\data.json';
+export const TAGSFILE = 'C:\\Users\\User\\WebstormProjects\\PsychInfo\\psych-info-ui\\src\\data\\tags.json';
