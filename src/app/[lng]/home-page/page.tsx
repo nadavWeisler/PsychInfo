97ec181f6291a476ae7ca80b2bf38fe7647607b1
Shlomi Shitrit <akunh113@gmail.com>
2023-12-22 00:37:22 +0200

    Add share button of the whole site Fixes #261

--- src/app/[lng]/home-page/page.tsx
+++ src/app/[lng]/home-page/page.tsx
@@ -6,21 +6,24 @@ import WelcomeMsg from "@/app/[lng]/Components/UI/WelcomeMsg";
 import { Filter } from "@/app/[lng]/general/interfaces";
 import Gallary from "@/app/[lng]/Components/gallary/gallary";
 import { GetFilters } from "@/app/[lng]/general/utils";
+import ShareDialog from "@/app/[lng]/Components/ResultComp/ShareDialog";
 import { Provider } from "react-redux";
 import store from "@/store";
 import { Fab } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
-import { fabStyle } from "./homePage.style";
+import { fabStyle, fabShareStyle } from "./homePage.style";
 import useTrans from "../hooks/useTrans";
 import { AuthContext } from "@/app/[lng]/context/AuthContext";
 import { useRouter } from "next/navigation";
 import { auth } from "../firebase/app";
+import ShareIcon from '@mui/icons-material/Share';
 
 export default function HomePage() {
   const [open, setOpen] = useState(false);
   const filters: Filter[] = GetFilters();
   const { user } = useContext(AuthContext);
   const [authUser, setAuthUser] = useState<User | null>(null);
+  const [openShare, setOpenShare] = useState<boolean>(false);
 
   const { i18n } = useTrans();
 
@@ -37,6 +40,14 @@ export default function HomePage() {
         <WelcomeMsg openWizradHandler={() => setOpen(true)} />
         <Wizrad open={open} onClose={() => setOpen(false)} />
         <Gallary filters={filters} />
+        <Fab
+            color="primary"
+            sx={fabShareStyle}
+            onClick={() => setOpenShare(true)}
+            aria-label="share"
+          >
+            <ShareIcon />
+          </Fab>
         {authUser && (
           <Fab
             color="primary"
@@ -47,6 +58,11 @@ export default function HomePage() {
             <AddIcon />
           </Fab>
         )}
+        <ShareDialog 
+            open={openShare}
+            onClose={() => setOpenShare(false)}
+            urlToShare={"https://www.psychinfo.co.il"}
+          />
       </Provider>
     </>
   );
