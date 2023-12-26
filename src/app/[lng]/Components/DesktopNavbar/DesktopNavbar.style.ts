1c5c5f3ae084d08674d8f6051fc4577c8db04d52
Shlomi Shitrit <akunh113@gmail.com>
2023-12-26 17:59:07 +0200

    Fix direction of navbar when change to lang with ltr direction Fixes #291

--- src/app/[lng]/Components/DesktopNavbar/DesktopNavbar.style.ts
+++ src/app/[lng]/Components/DesktopNavbar/DesktopNavbar.style.ts
@@ -6,12 +6,20 @@ export const styles = {
         alignItems: "center",
         justifyContent: "flex-start",
     },
-    desktopSelect: {
+    desktopSelectRtl: {
         color: "white",
         mr: 30,
         border: 0,
         position: "absolute",
         left: 0,
         ml: 2,
+    },
+    desktopSelectLtr: {
+        color: "white",
+        ml: 30,
+        border: 0,
+        position: "absolute",
+        right: 0,
+        mr: 2,
     },
 };
