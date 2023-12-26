2935b455b7e5ec2f3c309e58a7384284e9afe9d7
Shlomi Shitrit <akunh113@gmail.com>
2023-12-26 17:07:31 +0200

    Fix locale change not changing the route Fixes #242

--- src/app/[lng]/Components/LanguageSelect/LanguageSelect.tsx
+++ src/app/[lng]/Components/LanguageSelect/LanguageSelect.tsx
@@ -1,13 +1,25 @@
-import { Select, MenuItem } from "@mui/material";
+import { Select, MenuItem, SelectChangeEvent } from "@mui/material";
 import { DisplayLanguages } from "@/app/[lng]/general/interfaces";
 import { styles } from "@/app/[lng]/Components/LanguageSelect/LanguageSelect.style";
 import useTrans from "@/app/[lng]/hooks/useTrans";
+import { useRouter, usePathname } from "next/navigation";
 
 export default function LanguageSelect() {
     const { i18n } = useTrans();
+    const router = useRouter();
+    const pathname = usePathname();
+    const changeLanguage = (e: SelectChangeEvent<string>) => {
+        const newLang = e.target.value as string;
+        i18n.changeLanguage(newLang);
+        const pathNameArray = pathname.split("/");
+        pathNameArray[1] = newLang;
+        const newPathname = pathNameArray.join("/");
+        router.replace(newPathname);
+    };
+
     return (
         <Select
-            onChange={(e) => i18n.changeLanguage(e.target.value as string)}
+            onChange={changeLanguage}
             aria-label="change language"
             value={i18n.language || "he"}
             sx={styles.select}
